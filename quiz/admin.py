261d2d99624c9765f8afcc92c832dd36f5512ace
jungwoochang <woochang4862@gmail.com>
2019-11-27 15:35:19 +0900

    m

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from quiz.models import Quiz, Category
 
-# Register your models here.
+admin.site.register(Quiz)
+admin.site.register(Category)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+#from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date=models.DateTimeField('-date published')
+
+    def __str__(self):
+        return self.name
+
+class Quiz(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    sentence = models.CharField(max_length=200)
+    score = models.IntegerField(default=0)
+    #choices = ArrayField(models.CharField(max_length=200))
+    answer = models.IntegerField(default=0)
+    pub_date=models.DateTimeField('-date published')
+
+    def __str__(self):
+        return self.sentence
